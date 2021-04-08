5d70b8b99c1fd4296aaa9b04b1de541c5412d07a
unknown <45325278+lakshaysethi@users.noreply.github.com>
2021-04-08 23:26:11 +1200

    save commit

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 // ti = task input
 const ti = document.getElementById('main-todo-input')
+let taskList = []
 class Task{
     constructor(name='no name task',done ) {
         this.name = name;
         this.done = false;
-      }
+    }
 
 }
+
 ti.addEventListener('keyup',(ev)=>{
-    if(ev.key == 'Enter'){
+    if(ev.key == 'Enter'&& ti.value!=''){
         let task = ti.value
         addTaskToList(task)
-        ti.value = ''
+        refreshTaskList()
+        ti.value = 'enter new task'
+        ti.select()
     }
 })
 
 const addTaskToList = (task)=>{
+    taskList.push(task)
+}
 
+const refreshTaskList=()=>{
+    document.createElement(`<div class="list-item">
+    <input class='done' type="checkbox"><p>do abc</p>
+</div>`)
 }
