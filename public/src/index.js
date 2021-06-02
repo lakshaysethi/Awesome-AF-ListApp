d0f900b73d031dfdeb7109442fd9f9d36917e5ae
unknown <45325278+lakshaysethi@users.noreply.github.com>
2021-06-02 18:50:28 +1200

    added localstorage - persistance

--- public/src/index.js
+++ public/src/index.js
@@ -1,3 +1,16 @@
+/******************************************copied code */
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+/***************************************** copied code end */
+
+let all_list_items_array =[]
+
+
 const myinput = document.getElementById('myinput')
 
 myinput.focus()
@@ -8,6 +21,8 @@ myinput.addEventListener('keypress',(e)=>{
     if(e.key == 'Enter'){
         if(myinput.value !== ''){
             addNewListItem(myinput.value)
+            all_list_items_array.push(myinput.value)
+            localStorage.setObject("all_list_items", all_list_items_array); 
             myinput.value = ''
         }
     }
@@ -23,3 +38,38 @@ function addNewListItem(text){
     newlistitem.innerText=text
     listholder.appendChild(newlistitem)
 }
+
+
+
+
+
+const clearlistbtn = document.getElementById('clear-list')
+clearlistbtn.addEventListener('click',()=>{
+    if(confirm('are you sure you want to delete all items from the list?')){
+        all_list_items_array = []
+        localStorage.setObject("all_list_items", all_list_items_array); 
+        alert('done you can now reload the page to start a new list')
+        
+    }    
+})
+
+
+
+
+
+
+/****************************************** local storage*/
+//set things from local storage 
+if(localStorage.getObject("all_list_items")){
+    all_list_items_array=localStorage.getObject("all_list_items");
+    all_list_items_array.forEach(list_item => {
+        addNewListItem(list_item)
+    });
+}
+
+
+window.onbeforeunload = function(event) {
+    localStorage.setObject("all_list_items", all_list_items_array); 
+};
+/******************************************local storage end */
+
