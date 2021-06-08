c32fcbdc300dec9fa4ff09f88b179c0411024fb3
unknown <45325278+lakshaysethi@users.noreply.github.com>
2021-06-08 16:45:55 +1200

    fix archive functonality
    also removed copied code for local srorage

--- public/src/index.js
+++ public/src/index.js
@@ -1,28 +1,39 @@
-/******************************************copied code */
-Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
-}
-Storage.prototype.getObject = function(key) {
-    var value = this.getItem(key);
-    return value && JSON.parse(value);
-}
-/***************************************** copied code end */
-
-let all_list_items_array =[]
-
-
+let all_list_items ={} ;
+all_list_items.array = []
+let all_archived_items ={};
+all_archived_items.array =[]
 const myinput = document.getElementById('myinput')
+const listholder = document.getElementById('items-holder')
+
+if(localStorage.getItem('all_list_items')){
+    all_list_items = JSON.parse (localStorage.getItem('all_list_items'))
+    all_list_items.array.forEach(list_item => {
+        addNewListItem_toDOM(list_item)
+    });
+    
+
+}
+if(localStorage.getItem('all_archived_items')){
+    all_archived_items = JSON.parse (localStorage.getItem('all_archived_items'))
+}
+
+window.onbeforeunload = function(event) {
+    localStorage.setItem('all_archived_items', JSON.stringify(all_archived_items)); 
+    localStorage.setItem('all_list_items',JSON.stringify(all_list_items))
+
+};
+        
+
 
 myinput.focus()
 
 
 myinput.addEventListener('keypress',(e)=>{
-    console.log(myinput.value !== '')
+    
     if(e.key == 'Enter'){
         if(myinput.value !== ''){
-            addNewListItem(myinput.value)
-            all_list_items_array.push(myinput.value)
-            localStorage.setObject("all_list_items", all_list_items_array); 
+            all_list_items.array.push(myinput.value)
+            addNewListItem_toDOM(all_list_items.array[all_list_items.array.length-1])
             myinput.value = ''
         }
     }
@@ -30,8 +41,8 @@ myinput.addEventListener('keypress',(e)=>{
 
 
 
-const listholder = document.getElementById('items-holder')
-function addNewListItem(text){
+
+function addNewListItem_toDOM(text,showArchieveButton=true){
     let newlistitem = document.createElement('div')
     newlistitem.classList.add ( 'list-item','noselect')
     newlistitem.addEventListener ('click',()=>newlistitem.classList.toggle('checked'))
@@ -42,26 +53,41 @@ function addNewListItem(text){
     archive_icon_img.src  = 'https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_archive_48px-512.png'
     archive_icon_img.className = 'archive_icon_img'
     archive_icon.appendChild(archive_icon_img)
-    newlistitem.appendChild(archive_icon)
-    listholder.appendChild(newlistitem)
+    if(showArchieveButton) newlistitem.appendChild(archive_icon)
     archive_icon.addEventListener('click',()=>{
-        
+        let item_to_archieve = all_list_items.array.pop(all_list_items.array.indexOf(text))
+        all_archived_items.array.push(item_to_archieve)
+        updateDOM(all_list_items)
     })
+    
+    
+    
     listholder.prepend(newlistitem)
 }
 
 
 
-
-
-const clearlistbtn = document.getElementById('clear-list')
-clearlistbtn.addEventListener('click',()=>{
-    if(confirm('are you sure you want to delete all items from the list?')){
-        all_list_items_array = []
-        localStorage.setObject("all_list_items", all_list_items_array); 
-        alert('done you can now reload the page to start a new list')
+function updateDOM(items,showArchieveButton = true){
+    // TODO chaneg later : copy paste code
+    listholder.innerHTML =''
+    items.array.forEach(list_item => {
+        addNewListItem_toDOM(list_item,showArchieveButton)
+    });
+   
+   
         
-    }    
+}
+    
+const show_archieve_button = document.getElementById('clear-list')
+show_archieve_button.addEventListener('click',()=>{
+    // if(confirm('are you sure you want to delete all items from the list?')){
+    //     all_list_items.array = []
+    //     localStorage.setItem("all_list_items", JSON.stringify(all_list_items)); 
+    //     alert('done you can now reload the page to start a new list')
+        
+    // }   
+    
+    updateDOM(all_archived_items,false)
 })
 
 
@@ -69,20 +95,6 @@ clearlistbtn.addEventListener('click',()=>{
 
 
 
-/****************************************** local storage*/
-//set things from local storage 
-if(localStorage.getObject("all_list_items")){
-    all_list_items_array=localStorage.getObject("all_list_items");
-    all_list_items_array.forEach(list_item => {
-        addNewListItem(list_item)
-    });
-}
-
-
-window.onbeforeunload = function(event) {
-    localStorage.setObject("all_list_items", all_list_items_array); 
-};
-/******************************************local storage end */
 
 
 
