8e8d2127f0c92fa0dfc77693ba980cc54471176a
unknown <45325278+lakshaysethi@users.noreply.github.com>
2021-06-09 17:27:54 +1200

    added unarchive button to archived list

--- public/src/index.js
+++ public/src/index.js
@@ -53,7 +53,19 @@ function addNewListItem_toDOM(text,showArchieveButton=true){
     archive_icon_img.src  = 'https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_archive_48px-512.png'
     archive_icon_img.className = 'archive_icon_img'
     archive_icon.appendChild(archive_icon_img)
-    if(showArchieveButton) newlistitem.appendChild(archive_icon)
+    if(showArchieveButton) {
+        newlistitem.appendChild(archive_icon)
+    }else{
+        let unarchive_icon = archive_icon
+        unarchive_icon.classList.add('upside-down')
+        newlistitem.appendChild(unarchive_icon)
+        unarchive_icon.addEventListener('click',()=>{
+            let item_to_un_archieve = all_archived_items.array.pop(all_archived_items.array.indexOf(text))
+            all_list_items.array.push(item_to_un_archieve)
+            updateDOM(all_archived_items,false)
+        })
+        return listholder.prepend(newlistitem)
+    }
     archive_icon.addEventListener('click',()=>{
         let item_to_archieve = all_list_items.array.pop(all_list_items.array.indexOf(text))
         all_archived_items.array.push(item_to_archieve)
